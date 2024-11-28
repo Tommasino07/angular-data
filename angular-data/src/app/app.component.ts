da72ca0c06b1cc78a744968b133b28583bfb8e49
Tommaso Mattioni <mattionitommaso09@gmail.com>
2024-11-28 09:52:40 +0000

    angular-data

---
+++ angular-data/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { UserItemComponent } from './user-item/user-item.component';
+import { HelloWorldComponent } from './hello-world/hello-world.component';
+import { UserListComponent } from './user-list/user-list.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, UserItemComponent,HelloWorldComponent, UserListComponent ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'angular-data';
+}
